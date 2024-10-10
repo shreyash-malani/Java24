f6d977940dd377aa9f075644192b3b8ea1a96028
Shreyash Malani <160009112+shreyash-malani@users.noreply.github.com>
2024-10-10 19:25:11 +0530

    Add files via upload

---
+++ A4_App4.java
@@ -0,0 +1,12 @@
+public class A4_App4 
+{
+    public static void main(String [] args)
+    {
+        int i=1;
+        while(i<=5)
+        {
+            System.out.println("Welcome" + i);
+            i++;
+        }
+    }
+}
