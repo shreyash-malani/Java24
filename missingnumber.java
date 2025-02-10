8e59fe873cf2cfa77c11e2f03b01c8a8366d1516
Shreyash Malani <160009112+shreyash-malani@users.noreply.github.com>
2025-02-10 20:43:02 +0530

    Add files via upload

--- missingnumber.java
+++ missingnumber.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
-class Solution {
+class missingnumber {
     public static int missingNumber(int[] nums) {
-        int n = nums.length; // Length of the array
-        int expectedSum = n * (n + 1) / 2; // Sum of numbers from 0 to n
+        int n = nums.length; 
+        int expectedSum = n * (n + 1) / 2; 
         int actualSum = 0;
 
-        // Calculate the sum of all elements in the array
-        for (int num : nums) {
+       
+        for (int num : nums) 
+        {
             actualSum += num;
         }
 
-        // The missing number is the difference between the expected and actual sums
+        
         return expectedSum - actualSum;
     }
     public static void main(String [] args)
@@ -28,7 +29,39 @@ class Solution {
         }
 
         int result= missingNumber(a);
-        System.out.println("The union of the arrays is:");
-        System.out.println("Result" +result);
+        System.out.println("Result: " +result);
     }
 }
+
+// class missingnumber {
+//     public static int missingNumber(int[] nums) {
+//         int n = nums.length; // Length of the array
+//         int maxElement = n + 1; // The range is from 1 to n+1
+//         int expectedSum = maxElement * (maxElement + 1) / 2; // Sum of numbers from 1 to n+1
+//         int actualSum = 0;
+
+//         // Calculate the sum of all elements in the array
+//         for (int num : nums) {
+//             actualSum += num;
+//         }
+
+//         // The missing number is the difference between the expected and actual sums
+//         return expectedSum - actualSum;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the size of an array");
+//         int n = sc.nextInt();
+
+//         int[] a = new int[n];
+//         System.out.println("Enter " + n + " elements:");
+//         for (int i = 0; i < n; i++) {
+//             a[i] = sc.nextInt();
+//         }
+
+//         int result = missingNumber(a);
+//         System.out.println("Result: " + result);
+//     }
+// }
+
