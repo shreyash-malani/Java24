32f560168737895486725859a188e0523fa9a999
Shreyash Malani <160009112+shreyash-malani@users.noreply.github.com>
2025-06-13 20:40:12 +0530

    Add files via upload

---
+++ Pattern_2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Pattern_2
+{
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++) //reperesent the number of rows
+        {
+            for(int j=1;j<=i;j++) //represent the number of columns
+            {
+                System.out.print("* "); //print star
+            }
+            System.out.println(); 
+        }
+        sc.close();
+    }
+}
