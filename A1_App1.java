18531b5d42c5ba0512c7f4e28a96c1f77832f0b4
shreyash-malani <malanishreyash24@gmail.com>
2026-01-31 13:43:14 +0530

    Some changes made

--- A1_App1.java
+++ A1_App1.java
@@ -3,5 +3,6 @@ public class A1_App1
     public static void main(String [] args)
     {
         System.out.println("Welcome to Java Programming");
+        System.out.println("Hello making changes for github");
     }
 }
