26a728de8baf7b4b75a58e0a74088a17155aa807
shreyash-malani <malanishreyash24@gmail.com>
2026-05-06 21:45:49 +0530

    updated1

--- pattern6.java
+++ pattern6.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class pattern6 {
+public class Pattern6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
