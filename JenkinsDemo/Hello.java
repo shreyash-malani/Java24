fc609bebd56d4851f8edea39bc02987529ff6155
shreyash-malani <malanishreyash24@gmail.com>
2026-05-31 23:58:05 +0530

    Jenkins Project part 3

--- JenkinsDemo/Hello.java
+++ JenkinsDemo/Hello.java
@@ -1,6 +1,7 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello Jenkins");
-        System.out.println("This is a demo for Jenkins");
+    public class Hello {
+        public static void main(String[] args) {
+            System.out.println("Hello Jenkins");
+            System.out.println("This is a demo for Jenkins");
+            System.out.println("This is a demo for Jenkins how it feels to see RCB wining the IPL 2026");
+        }
     }
-}
